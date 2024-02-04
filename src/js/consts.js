9f6c684c061b3698c36c9af3f9bf95269a36a77d
Vladislav <145756878+amen192315@users.noreply.github.com>
2024-02-04 17:45:52 +0300

    Update consts.js

--- src/js/consts.js
+++ src/js/consts.js
@@ -1,5 +1,5 @@
 //  export const WORDS = ['code', 'frontend','programming']
-export const WORDS = ['влад','ева','вадим','рома','алик', 'анна', 'мария', 'виктория','иван', 'данил', 'дмитрий', 'кирилл', 'егор', 'илья','алексей','настя']
+export const WORDS = ['влад','ева','вадим','рома','алик', 'анна', 'мария', 'виктория','иван', 'данил', 'дмитрий', 'кирилл', 'егор', 'илья','алексей','настя','ульяна']
 
 // export const KEYBOARD_LETTERS = Array.from('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
 export const KEYBOARD_LETTERS = Array.from('ЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮ')
